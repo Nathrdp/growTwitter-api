40f693221fea4bf9c9d87448a5d0848a1d1bf900
Nathrdp <nathaliardpaula@gmail.com>
2026-05-04 22:09:06 -0300

    feat: initialize Prisma client with PostgreSQL adapter in dist directory

--- dist/database/prisma.js
+++ dist/database/prisma.js
@@ -4,9 +4,10 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
 };
 Object.defineProperty(exports, "__esModule", { value: true });
 const adapter_pg_1 = require("@prisma/adapter-pg");
-const client_1 = require("@prisma/client");
+const client_1 = __importDefault(require("@prisma/client"));
+const { PrismaClient } = client_1.default;
 const dotenv_1 = __importDefault(require("dotenv"));
 dotenv_1.default.config();
 const adapter = new adapter_pg_1.PrismaPg({ connectionString: process.env.DATABASE_URL });
-const prisma = new client_1.PrismaClient({ adapter });
+const prisma = new PrismaClient({ adapter });
 exports.default = prisma;
