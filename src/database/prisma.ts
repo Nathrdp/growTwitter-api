694920dc0622585880fd32bec910eb6652a8f73f
Nathrdp <nathaliardpaula@gmail.com>
2026-05-04 22:14:08 -0300

    feat: initialize Prisma client with pg adapter and environment configuration

--- src/database/prisma.ts
+++ src/database/prisma.ts
@@ -1,10 +1,10 @@
 import { PrismaPg } from "@prisma/adapter-pg";
-import prisma_pkg from "@prisma/client";
-const { PrismaClient } = prisma_pkg;
+import * as prismaClient from "@prisma/client";
 import dotenv from "dotenv";
 
 dotenv.config();
 
+const PrismaClient = (prismaClient as any).PrismaClient;
 const adapter = new PrismaPg({ connectionString: process.env.DATABASE_URL });
 const prisma = new PrismaClient({ adapter });
 
